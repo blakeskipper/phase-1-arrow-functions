a57d40f9b4d350190bbf5c49ef1c4eb584a74ba1
blakeskipper <blakeskipper@gmail.com>
2022-03-17 12:31:22 -0400

    commit tree 036fbd3f5eb5457da5f0a75a6fcf882b157793bf

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const divide = (num1,num2) => 2000/100
+
+let square = x => x * x;
+
+let add = (a, b) => a + b;
+
+
+
+
+
+
+// const divide = (parameter1, parameter2) => parameter1 / parameter2;
+// divide(2000, 100);
+
+
+
+// const divide = (parameter1, parameter2) => parameter1 / parameter2;
+// divide(2000,100); 
+// let divide = 2000/100;
